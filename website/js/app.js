10a965d3e8fee1cfa2d674a4c9d3070bdd30f21d
Kymii <65640023+Kymii@users.noreply.github.com>
2021-02-23 14:02:52 -0500

    Add comments

--- website/js/app.js
+++ website/js/app.js
@@ -1,14 +1,19 @@
+// api url and key 
 const apiURL = 'https://api.openweathermap.org/data/2.5/weather?zip=';
 const apiKey = '&units=imperial&appid=fd5d23547e69d0663dbc0e5f3f183640';
 
+// grab button element to generate output
 const generate = document.getElementById('generate');
 
+// event listener for generate button 
 generate.addEventListener('click', (e) => {
     e.preventDefault();
 
+    // gets zip code and feeling values from input and textarea 
     const zip = document.getElementById('zip').value;
     const feelings = document.getElementById('feelings').value;
 
+    // function getWeather fetches api data using given url, zip and key then assigns data to variables
     getWeather(apiURL, zip, apiKey).then( (weatherData) => {
         try {
         const temp = weatherData.main.temp;
@@ -19,7 +24,8 @@ generate.addEventListener('click', (e) => {
         const wind = weatherData.wind.speed;
         const pressure = weatherData.main.pressure;
         const date = weatherData.dt;
-
+        
+        // USe POSt route '/save' post data to the server then using GET route '/grab' update UI 
         postWeather('/save', {
             temp,
             city,
@@ -37,6 +43,7 @@ generate.addEventListener('click', (e) => {
     })
 });
 
+//grabs data from api
 const getWeather = async (apiURL, zip, apiKey) => {
     const request = await fetch (apiURL + zip + apiKey)
     try {
@@ -48,6 +55,7 @@ const getWeather = async (apiURL, zip, apiKey) => {
     }
 };
 
+// async function to post weather data
 const postWeather = async (url, data) => {
     console.log(data)
     await fetch(url, {
@@ -58,17 +66,24 @@ const postWeather = async (url, data) => {
     });
 };
 
+// update UI using an async function to fetch the data 
 const updateUI = async () => {
     const request = await fetch('/grab');
     try {
         const last = await request.json();
+
+        //update UI elements
         document.getElementById('temp').innerHTML = `${Math.floor(last.temp)}<span>&#176;</span>F`;
         document.getElementById('weather').textContent = last.weather;
         document.getElementById('city').textContent = last.city;
+
+        // icon image link
         let imglink = 'http://openweathermap.org/img/wn/' + last.icon + '@2x.png';
         document.getElementById('weatherIcon').innerHTML = `<img alt="weather icon" src=${imglink}></img>`;
+
         const card_list = ['card-one', 'card-two', 'card-three'];
         
+        // loop through each card to display once data is retrieved
         card_list.forEach(card => {
             document.getElementById(card).classList.remove('hide');
         })
@@ -76,6 +91,8 @@ const updateUI = async () => {
         document.getElementById('humidity').textContent = `${last.humidity}%`;
         document.getElementById('wind').textContent = `${last.wind} mph`;
         document.getElementById('pressure').textContent = `${last.pressure} hpa`;
+
+        //convert date value from api
         let today = new Date(last.date * 1000).toLocaleDateString('en-US');
         document.getElementById('date').textContent = `Todays Date: ${today}`;
         document.getElementById('userFeelings').textContent = `Mood: ${last.feelings}`;
